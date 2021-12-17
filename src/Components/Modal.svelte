40f17aeb462bd0e6df18c3eedf84695becc1a154
Priya Tiwari <92025463+uc-prt12@users.noreply.github.com>
2021-12-17 13:13:02 +0530

    Update Modal.svelte

--- src/Components/Modal.svelte
+++ src/Components/Modal.svelte
@@ -1,16 +1,15 @@
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+<div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header border-0">
-                <h5 class="modal-title" id="exampleModalLabel">XML</h5>
+                <slot name="modal_header"></slot>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
             </div>
             <div name="modal_body" class="modal-body" id="xml_data">
                 <slot name="modal_body"></slot>
             </div>
             <div class="modal-footer border-0">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
-                <button type="button" class="btn btn-primary">DONE</button>
+                <slot name="modal_footer"></slot>
             </div>
         </div>
     </div>
