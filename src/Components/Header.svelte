d5ce84022f6b29473c2ff2f6410334ab0a4fbc1d
Priya Tiwari <92025463+uc-prt12@users.noreply.github.com>
2021-12-17 13:13:33 +0530

    Update Header.svelte

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -1,5 +1,6 @@
 <script>
     export let menu;
+    export let default_xml;
 </script>
 <nav class="border-bottom">
     <img class="img-fluid" src="http://localhost/pe-gold3/layout/themes/bootstrap4/images/logo/ucertify_logo.png" alt="ucertify_logo"/>
@@ -8,7 +9,7 @@
             TOOLS
         </span>
         <ul class="dropdown-menu position-absolute top-0 pt-2" aria-labelledby="dropdownMenuLink">
-            <li class="dropdown-item py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <li class="dropdown-item py-2" data-bs-toggle="modal" data-bs-target="#exampleModal" >
                 XML
             </li>
             <li class="dropdown-item py-2">Show Algorithmic</li>
@@ -24,3 +25,20 @@
     <button class="btn btn-primary version p-0 px-1">V2.0</button>
     {/if}
 </nav>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header border-0">
+                <h3>XML</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+            </div>
+            <div name="modal_body" class="modal-body" id="xml_data">
+                {default_xml}
+            </div>
+            <div class="modal-footer border-0">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CANCEL</button>
+                <button type="button" class="btn btn-primary">DONE</button>
+            </div>
+        </div>
+    </div>
+</div>
